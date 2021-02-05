d88ed83c78ee7e6d522d2fb0e8147e90fb436a2e
Matt Winkler <matt.winkler@fishtownanalytics.com>
2021-02-05 17:13:42 +0000

    add tests

---
+++ models/staging/stripe/stg_payments.sql
@@ -0,0 +1,11 @@
+
+with source as (select * from {{ source('stripe', 'payment') }} )
+
+select
+  id as payment_id,
+  orderid as order_id,
+  paymentmethod as payment_method,
+  status,
+  amount,
+  created as created_at
+from source
