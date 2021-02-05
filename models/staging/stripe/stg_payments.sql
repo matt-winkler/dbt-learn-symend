5114f427e95834ce415f106eda5b00e7c3d4f786
Matt Winkler <matt.winkler@fishtownanalytics.com>
2021-02-05 18:18:02 +0000

    configure sources and documentation

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -1,11 +1,12 @@
 
 with source as (select * from {{ source('stripe', 'payment') }} )
 
-select
+select 
   id as payment_id,
   orderid as order_id,
   paymentmethod as payment_method,
   status,
-  amount,
+  amount / 100 as amount,
   created as created_at
+  
 from source
